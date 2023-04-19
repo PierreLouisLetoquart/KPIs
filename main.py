b41aeec569e5d3259f8faaa9e4fb0a495916165d
PILOU <pierrelouislet@gmail.com>
2023-04-19 16:05:09 +0200

    wip: make some tests

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import setup
-from interfaces.menu import main_menu
+from utils.supabaseDataloader import fetch_orders, store_data_to_csv
+from utils.supabaseClient import create_supabase_client
 
 # Point d'entrée du programme
 if __name__ == "__main__":
-    main_menu()
+    logs = fetch_orders(create_supabase_client(), 10)
+    store_data_to_csv(logs, "data/logs2.csv")
