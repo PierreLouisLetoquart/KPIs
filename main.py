959cc8f61c449b2bd387f9335436152d024a20ca
PILOU <pierrelouislet@gmail.com>
2023-04-20 13:47:56 +0200

    fix: update kpis logs folder name and make an example of using it

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import setup
+from utils.supabaseDataloader import fetch_logs, store_data_to_csv
+from utils.supabaseClient import create_supabase_client
+from utils.logKpis import count_records_by_type
+
+def example():
+    filename = "data/logs.csv"
+    client = create_supabase_client()
+
+    dataframe = fetch_logs(client, 'cotele')
+    store_data_to_csv(dataframe, filename)
+
+    output = count_records_by_type(filename)
+    print(output)
 
 # Point d'entrée du programme
 if __name__ == "__main__":
-    setup.setup()
-    print("Hello World !")
+    example()
